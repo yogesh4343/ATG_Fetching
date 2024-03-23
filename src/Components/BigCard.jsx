533035e7fcab5986a29fbe399b88fe0ac4914651
yogesh <103855728+yogesh4343@users.noreply.github.com>
2024-03-23 07:11:41 +0530

    Update BigCard.jsx

--- src/Components/BigCard.jsx
+++ src/Components/BigCard.jsx
@@ -4,13 +4,13 @@ const BigCard = ({ firstName, lastName, username, jobTitle, email, Bio, avatar }
   return (
     <div>
       <div className="div2">
-        <div className="w-[40vw] hidden   bg-[#979597] text-white top-[9rem] p-12 gap-6  lg:flex flex-col  font-[system-ui]   ">
+        <div className="w-[40vw] hidden  text-left bg-[#f7f5f9] text-gray-500 top-[9rem] p-12 gap-6  lg:flex flex-col  font-[system-ui]   ">
           <img className="w-44 h-44 " src={avatar} alt="img" />
 
           <div className="name flex  gap-2   text-[20px] ">
             <span className="text-black  text-[20px] ">Full Name : </span>
-            <div className="first text-white">{firstName}</div>
-            <div className="last text-white ">{lastName}</div>
+            <div className="first text-gray-500">{firstName}</div>
+            <div className="last text-gray-500 ">{lastName}</div>
           </div>
 
           <div className="userName   text-[20px] ">
